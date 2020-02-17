01746d7f28d247e730599c43d168e634d2245e2d
Stefani King <themrsking@gmail.com>
2020-02-17 21:52:15 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,1 @@
-# # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-filter_parameter_loggingirb
-'greeting' = "hello World"
-#greeting = "hello World
-greeting = 'hello World'
+greeting= "Hello World"
